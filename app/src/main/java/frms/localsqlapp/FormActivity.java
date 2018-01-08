0fb54ea1099b2c3e47fa706c1eb48c1ec366a232
Apprenant <rico.dubus@hotmail.fr>
2018-01-08 15:15:05 +0100

    Insertion d'un contact dans la bDD

--- app/src/main/java/frms/localsqlapp/FormActivity.java
+++ app/src/main/java/frms/localsqlapp/FormActivity.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import fr.sm.datab.DataBase.DataBaseHandler;
 
@@ -43,6 +44,7 @@ public class FormActivity extends AppCompatActivity {
         insertValues.put("email", email);
         try{
         db.getReadableDatabase().insert("contacts", null,insertValues );
+            Toast.makeText()
 
 
             }
